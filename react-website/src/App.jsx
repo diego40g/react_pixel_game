f0dbbd79d481942fc9587628b2894844527a61f0
diego40g <diegopaznaula@hotmail.com>
2022-06-12 22:28:36 -0500

    add components

--- react-website/src/App.jsx
+++ react-website/src/App.jsx
@@ -1,18 +1,15 @@
 import * as React from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import Sidebar from "./components/Sidebar";
+import {BrowserRouter as Router} from 'react-router-dom';
 
 const App=()=> {
   return (
-    <>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path='/' exact></Route>
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Sidebar/>
+      <Navbar/>
+    </Router>
   )
 }
 
